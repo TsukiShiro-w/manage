8ec5d668a210f8916467693ffa11a909b1b57dee
w <710245672@qq.com>
2020-04-05 14:37:35 +0800

    登录页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,42 @@
 <template>
   <div id="app">
-    <el-row>
-      <el-button type="danger" @click="getRouter">危险按钮</el-button>
-    </el-row>
-    <router-view></router-view>
+    <div class="login">
+      <div class="left"></div>
+      <div class="right">
+        <img src="@/assets/img/login_banner_ele.png" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    getRouter(){
-      this.$router.push('/');
+    getRouter() {
+      this.$router.push("/");
     }
-  },
+  }
 };
 </script>
 
 
 <style lang='less'>
+@import url(~@/style/base.css);
 // 使用less,lang='less'
+.login {
+  display: flex;
+  background:linear-gradient(225deg,rgba(20,147,250,1),rgba(1,198,250,1));
+  justify-content: space-around;
+  align-items: center;
 
+  .left {
+    width: 478px;
+    height: 550px;
+    background: rgba(245, 245, 245, 1);
+  }
+
+  .right{
+
+  }
+}
 </style>
