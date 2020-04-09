d7fded3abc90cfbee2eae56e61903a5caea5832c
w <710245672@qq.com>
2020-04-09 19:01:18 +0800

    登录按钮跳转

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -131,6 +131,7 @@ export default {
             console.log(res);
             this.$message.success("登录成功");
             saveToken(res.data.token);
+            this.$router.push('/layout');
           });
         } else {
           this.$message.error("请输入必要的登录信息");
