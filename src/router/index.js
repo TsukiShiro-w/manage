98a86f7a183e48c23cb9c6250b5d1640e5e0e269
w <710245672@qq.com>
2020-04-09 16:45:59 +0800

    主页路由&主页头部布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,24 @@
 import Vue from 'vue';
 // 1.导入路由实例化对象
-import VueRouter from 'vue-router'
+import VueRouter from 'vue-router';
 
 // 导入子组件
 import login from '@/view/login/login.vue';
+import home from '@/view/home/layout.vue';
 // 2.注册路由
 Vue.use(VueRouter);
 
 // 路由实例化
-let router  = new VueRouter({
+let router = new VueRouter({
     // 路由匹配
-    routes:[
+    routes: [
         {
-            path:'/',
-            component:login,
+            path: '/',
+            component: login,
+        },
+        {
+            path: '/layout',
+            component: home,
         }
     ]
 });
---
+++ src/view/home/layout.vue
@@ -0,0 +1,76 @@
+<template>
+  <el-container class="layout">
+    <el-header class="header">
+      <div class="left">
+        <i class="el-icon-s-fold setIconsize"></i>
+        <img src="@/assets/img/login-logo.png" class="logo" alt />
+        <span class="title">黑马面面</span>
+      </div>
+      <div class="right">
+        <img src alt />
+        <span class="user">xxx,你好</span>
+        <el-button class="btnOut" type="primary">退出</el-button>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="201px" class="aside">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang='less'>
+.layout {
+  height: 100%;
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 2px 5px 0px rgba(63, 63, 63, 0.35);
+
+    .left {
+      line-height: 60px;
+
+      .setIconsize {
+        font-size: 20px;
+        margin-right: 22px;
+      }
+
+      .logo {
+        margin-right: 15px;
+      }
+
+      .title {
+        width: 92px;
+        height: 22px;
+        font-size: 22px;
+        font-family: Microsoft YaHei;
+        font-weight: bold;
+        color: rgba(73, 161, 255, 1);
+      }
+    }
+
+    .right {
+      line-height: 60px;
+      .btnOut {
+        margin-left: 20px;
+      }
+    }
+  }
+
+  .aside {
+    height: 100%;
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 2px 5px 0px rgba(63, 63, 63, 0.35);
+  }
+}
+</style>
